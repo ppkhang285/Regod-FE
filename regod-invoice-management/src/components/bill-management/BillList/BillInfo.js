b33aee632530fd38aef5d7c6aa1c21a04db928c7
Phạm Phú Khang <ppkhang285@gmail.com>
2024-11-03 10:55:56 +0700

    add Bill Table

---
+++ regod-invoice-management/src/components/bill-management/BillList/BillInfo.js
@@ -0,0 +1,15 @@
+
+
+function BillInfo(props){
+    return (
+        <tr>
+            <td>1</td>
+            <td>Department Name</td>
+            <td>Bill Name</td>
+            <td>200.000 VND</td>
+            <td>03/11/2024</td>
+            <td>Action</td>
+        </tr>
+    );
+}
+export default BillInfo;
--- regod-invoice-management/src/components/bill-management/BillList/BillList.js
+++ regod-invoice-management/src/components/bill-management/BillList/BillList.js
@@ -1,9 +1,29 @@
+import { Table } from "react-bootstrap";
+import BillInfo from "./BillInfo";
 
 
 function BillList(props){
     return(
-        <div>Hello</div>
+        <div>
+            <Table bordered={true} >
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Department Name</th>
+                        <th>Bill Name</th>
+                        <th>Total</th>
+                        <th>Due Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <BillInfo/>
+                </tbody>
+            </Table>
+        </div>
 
     );
 
 }
+
+export default BillList;
