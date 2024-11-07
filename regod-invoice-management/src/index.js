d36d254e0bc6436abf3250fe4ca6b70f95b89b4a
Phạm Phú Khang <ppkhang285@gmail.com>
2024-11-08 01:50:47 +0700

    Updater router

--- regod-invoice-management/src/index.js
+++ regod-invoice-management/src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
 import './index.css';
 import App from './routes/App';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/hello",
-    element: <p>Hello</p>
-  }
-]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    {/* <RouterProvider router={router} /> */}
     <App/>
   </React.StrictMode>
 );
